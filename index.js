9753071be11ddf965643d2e3e93908aa36a34ed1
Avi-17 <workhorseavi17@gmail.com>
2024-10-31 17:41:42 +0530

    fixed config

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+require('./config/db');
 app.use(bodyParser.json());
 const authRoutes = require('./routes/authRoutes');
 const fileRoutes = require('./routes/fileRoutes');
@@ -12,5 +13,3 @@ app.use('/api', fileRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
